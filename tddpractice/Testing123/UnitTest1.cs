d77c60ff1d648ae7e41492567541775f1666ff6b
wtnerb <wtnerb@gmail.com>
2018-03-21 12:20:27 -0700

    refactored tests to theories. Testing to make sure a withdrawl will not happen if balance would be negative.

--- tddpractice/Testing123/UnitTest1.cs
+++ tddpractice/Testing123/UnitTest1.cs
@@ -7,16 +7,19 @@ namespace Testing123
     public class UnitTest1
     {
         [Theory]
-        [InlineData(100.234)]
-        public void BalanceFormat(double balance)
+        [InlineData("$100.23", 100.234)]
+        [InlineData("$100.00", 100)]
+        public void BalanceFormat(string expected, double balance)
         {
-            Assert.Equal("$100.23" , GetBalance(balance));
+            Assert.Equal(expected, GetBalance(balance));
         }
 
-        [Fact]
-        public void CanWithdraw ()
+        [Theory]
+        [InlineData("$52.23", 100.234, 48)]
+        [InlineData("$100.23", 100.234, 408)]
+        public void CanWithdraw (string expected, double current, double withdrawl)
         {
-            Assert.Equal("$52.23", GetBalance(Withdraw(100.234, 48)));
+            Assert.Equal(expected, GetBalance(Withdraw(current, withdrawl)));
         }
     }
 }
